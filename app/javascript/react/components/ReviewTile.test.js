65cac3adf1bf7e68749c345b13c7ad7d3237b728
charles colfer <charlescolfer@users.noreply.github.com>
2019-10-18 13:18:41 -0400

    Refactor of ReviewTile Test

--- app/javascript/react/components/ReviewTile.test.js
+++ app/javascript/react/components/ReviewTile.test.js
@@ -1,9 +1,3 @@
-import React from "react"
-import Enzyme, { mount } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
-
-Enzyme.configure({ adapter: new Adapter() })
-
 import ReviewTile from "./ReviewTile"
 
 describe("ReviewTile", () => {
