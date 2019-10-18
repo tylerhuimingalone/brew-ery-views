b7b48639f75f6f6b8e0136e697846a8aadd5fcbd
Susan Ma <masusan29@gmail.com>
2019-10-18 17:21:50 -0400

    add delete functionality to reviews

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,18 +2,29 @@ class Api::V1::ReviewsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def index
-    render json: Review.where(brewery_id: params["brewery_id"])
+    brewery = Brewery.find(params["brewery_id"])
+    user_id = 0
+    if current_user
+      user_id = current_user.id
+    end
+    render json: { reviews: brewery.reviews, user_id: user_id }
   end
 
   def create
     new_review = Review.new(review_params)
     brewery = Brewery.find(params[:breweryId])
     new_review.brewery = brewery
+    new_review.user = current_user
     if new_review.save
       render json: new_review
     else
       render json: new_review.errors
     end
+  end
+
+  def delete
+    Review.find(params[:id]).destroy
+    render json: { message: "Review Deleted" }
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Brewery.create(
   city: "Boston",
   state: "MA",
   zip: "02114",
-  image: ""
+  image: "https://i2.wp.com/beerbusterspodcast.com/wp-content/uploads/2014/06/Beer-Works-Featured-2.jpg?fit=1300%2C846&ssl=1"
 )
 Brewery.create(
   name: "Night Shift Brewing",
@@ -12,7 +12,7 @@ Brewery.create(
   city: "Boston",
   state: "MA",
   zip: "02114",
-  image: ""
+  image: "https://www.lenoxhotel.com/wp-content/uploads/2017/05/NightShift.jpg"
 )
 Brewery.create(
   name: "Harpoon Brewery & Beer Hall",
@@ -20,7 +20,7 @@ Brewery.create(
   city: "Boston",
   state: "MA",
   zip: "02110",
-  image: ""
+  image: "https://www.wheretraveler.com/sites/default/files/images/harpoon_1658_gelsobello_2016.jpg"
 )
 Brewery.create(
   name: "Hopsters Brewing Company",
@@ -28,35 +28,47 @@ Brewery.create(
   city: "Boston",
   state: "MA",
   zip: "02110",
-  image: ""
+  image: "https://www.bostonseaport.xyz/wp-content/uploads/2018/06/boston-seaport-places-hopsters.jpg"
+)
+User.create(
+  username: 'username',
+  email: "user@example.com",
+  password: 'password',
+  password_confirmation: 'password'
 )
 Review.create(
   rating: 5,
   comment: "This place was absolutely fantastic, I had a great time with my friends at a great price.",
-  brewery_id: 1
+  brewery_id: 1,
+  user: User.first
 )
 Review.create(
   rating: 5,
   comment: "They had my favorite beer.",
-  brewery_id: 2
+  brewery_id: 2,
+  user: User.first
 )
 Review.create(
   rating: 4,
   comment: "Lots of seating!",
-  brewery_id: 2
+  brewery_id: 2,
+  user: User.first
 )
 Review.create(
   rating: 1,
   comment: "Waiter was a jerk!!!",
-  brewery_id: 3
+  brewery_id: 3,
+  user: User.first
 )
 Review.create(
   rating: 2,
   comment: "It took forever to get my food!",
-  brewery_id: 3
+  brewery_id: 3,
+  user: User.first
 )
 Review.create(
   rating: 3,
   comment: "Beer was sub-par.",
-  brewery_id: 4
+  brewery_id: 4,
+  user: User.first
 )
