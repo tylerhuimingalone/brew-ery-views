b16e66ca6142ac60def6a25ce366814e1ad84014
Susan Ma <masusan29@gmail.com>
2019-10-21 10:39:56 -0400

    update review test and form page for user authorization

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -11,14 +11,18 @@ class Api::V1::ReviewsController < ApplicationController
   end
 
   def create
-    new_review = Review.new(review_params)
-    brewery = Brewery.find(params[:breweryId])
-    new_review.brewery = brewery
-    new_review.user = current_user
-    if new_review.save
-      render json: new_review
+    if user_signed_in?
+      new_review = Review.new(review_params)
+      brewery = Brewery.find(params[:breweryId])
+      new_review.brewery = brewery
+      new_review.user = current_user
+      if new_review.save
+        render json: new_review
+      else
+        render json: new_review.errors
+      end
     else
-      render json: new_review.errors
+      render json: {user: "You must be signed in to add a review."}
     end
   end
 
