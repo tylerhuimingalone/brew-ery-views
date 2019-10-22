7e7c6b004aaaef96178f7f75288927b155c4cb4f
tylerhuimingalone <tylermingalone@gmail.com>
2019-10-22 16:25:56 -0400

    Add amin table to update user role

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,8 @@ Rails.application.routes.draw do
       resources :reviews, only: [:create, :destroy, :update]
     end
   end
+
+  namespace :admin do
+    resources :users, only: [:index, :update]
+  end
 end
